c380f772609a2728475fb4a7be43c0e9e9fa126c
harshitappperfect <harshit.purohit@AppPerfect.com>
2025-01-03 11:22:53 +0530

    feat: add book migration

---
+++ prisma/migrations/20250103053655_book/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "publishedAt" TIMESTAMP(3) NOT NULL,
+    "summary" TEXT NOT NULL,
+
+    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
+);
